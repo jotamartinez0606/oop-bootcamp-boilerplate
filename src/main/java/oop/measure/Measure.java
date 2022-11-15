fe3a9ce5c1d3ff87407756ba2d8e85496a4f0002
Jota Martínez <juan.martinez@thoughtworks.com>
2022-11-15 11:59:02 +0100

    added Inches to Yards and test

--- src/main/java/oop/measure/Measure.java
+++ src/main/java/oop/measure/Measure.java
@@ -2,8 +2,22 @@ package oop.measure;
 
 public class Measure {
 
-  public static double compareFeetToMeters(double feet) {
-    return feet * 0.3048; // todo convert this into a constant
+  private static final double FEET_TO_METERS = 0.3048;
+  private static final double INCHES_TO_YARDS = 0.0278;
+
+  public double compareFeetToMeters (double feet) {
+    return feetToMeters(feet);
   }
 
+  public double compareInchesToYards (double inches) {
+    return inchesToYards(inches);
+  }
+
+  private static double feetToMeters(double feet) {
+    return feet * FEET_TO_METERS;
+  }
+
+  private static double inchesToYards(double inches) {
+    return inches * INCHES_TO_YARDS;
+  }
 }
--- src/test/java/oop/measure/MeasureTest.java
+++ src/test/java/oop/measure/MeasureTest.java
@@ -6,12 +6,18 @@ import org.testng.annotations.Test;
 
 public class MeasureTest {
 
+  Measure measure = new Measure();
+
 
   @Test
   public void itShouldCompareFeetToMeters() {
-
-    assertEquals(Measure.compareFeetToMeters(5.0), 1.524);
-
+    assertEquals(measure.compareFeetToMeters(5.0), 1.524);
   }
+
+  @Test
+  public void itShouldCompareInchesToYards() {
+    assertEquals(measure.compareInchesToYards(1.0), 0.0278);
+  }
+
 
 }
