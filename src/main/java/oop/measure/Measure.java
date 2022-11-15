75e77040722e1c087b4a4cb0b4f6b6c08b92630a
Jota Martínez <juan.martinez@thoughtworks.com>
2022-11-15 12:24:02 +0100

    refactored class Measure and tests

--- src/main/java/oop/measure/Measure.java
+++ src/main/java/oop/measure/Measure.java
@@ -5,19 +5,12 @@ public class Measure {
   private static final double FEET_TO_METERS = 0.3048;
   private static final double INCHES_TO_YARDS = 0.0278;
 
-  public double compareFeetToMeters (double feet) {
-    return feetToMeters(feet);
-  }
-
-  public double compareInchesToYards (double inches) {
-    return inchesToYards(inches);
-  }
-
-  private static double feetToMeters(double feet) {
+  public static double compareFeetToMeters(double feet) {
     return feet * FEET_TO_METERS;
   }
 
-  private static double inchesToYards(double inches) {
+  public static double compareInchesToYards(double inches) {
     return inches * INCHES_TO_YARDS;
   }
+
 }
--- src/test/java/oop/measure/MeasureTest.java
+++ src/test/java/oop/measure/MeasureTest.java
@@ -6,17 +6,14 @@ import org.testng.annotations.Test;
 
 public class MeasureTest {
 
-  Measure measure = new Measure();
-
-
   @Test
   public void itShouldCompareFeetToMeters() {
-    assertEquals(measure.compareFeetToMeters(5.0), 1.524);
+    assertEquals(Measure.compareFeetToMeters(5.0), 1.524);
   }
 
   @Test
   public void itShouldCompareInchesToYards() {
-    assertEquals(measure.compareInchesToYards(5.0), 0.1389, 0.01);
+    assertEquals(Measure.compareInchesToYards(5.0), 0.1389, 0.01);
   }
 
 
